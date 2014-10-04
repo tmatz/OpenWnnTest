b61cdeb00695aa6b7913abbccc20a64ace199c6e
tmatz <t.matsuda.1000@gmail.com>
2014-10-04 16:29:49 +0900

    漢字変換と候補の取得方法を確認

--- src/com/tmatz/openwnntest/MainActivity.java
+++ src/com/tmatz/openwnntest/MainActivity.java
@@ -4,14 +4,129 @@ import android.app.*;
 import android.os.*;
 import android.view.*;
 import android.widget.*;
+import jp.co.omronsoft.openwnn.OpenWnnDictionaryImpl;
+import jp.co.omronsoft.openwnn.JAJP.OpenWnnEngineJAJP;
+import jp.co.omronsoft.openwnn.WnnEngine;
+import jp.co.omronsoft.openwnn.WnnWord;
+import jp.co.omronsoft.openwnn.ComposingText;
+import jp.co.omronsoft.openwnn.StrSegment;
+import jp.co.omronsoft.openwnn.JAJP.Romkan;
+import jp.co.omronsoft.openwnn.LetterConverter;
+import jp.co.omronsoft.openwnn.CandidateFilter;
 
 public class MainActivity extends Activity
 {
+	private OpenWnnEngineJAJP mConvEngine;
+	private final CandidateFilter mCandidateFilter = new CandidateFilter();
+	private final LetterConverter mPreConv = new Romkan();
+	private final ComposingText mText = new ComposingText();
+	private TextView mTextView;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
 	{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-    }
+		
+		mTextView = (TextView)findViewById(R.id.text_view);
+
+		try
+		{
+			mConvEngine = new OpenWnnEngineJAJP(this, "/data/data/com.tmatz.openwnntest/writableJAJP.dic");
+			mConvEngine.init();
+			mConvEngine.setFilter(mCandidateFilter);
+			mConvEngine.setKeyboardType(OpenWnnEngineJAJP.KEYBOARD_QWERTY);
+			//mConvEngine.setDictionary(OpenWnnEngineJAJP.DIC_LANG_JP);
+
+			insert("k");
+			insert("a");
+			insert("n");
+			insert("j");
+			insert("i");
+			insert("w");
+			insert("o");
+			insert("h");
+			insert("e");
+			insert("n");
+			insert("k");
+			insert("a");
+			insert("n");
+			insert("n");
+			insert("s");
+			insert("i");
+			insert("m");
+			insert("a");
+			insert("s");
+			insert("u");
+			//insert(".");
+			
+			mText.setCursor(ComposingText.LAYER1, 0);
+
+			int res;
+			res = mConvEngine.convert(mText);
+			//res = mConvEngine.predict(mText, 0, -1);
+			show("num = " + res);
+			if (mText.size(ComposingText.LAYER1) > 0)
+			{
+				String str = mText.toString(ComposingText.LAYER1, 0, 0);
+				for (int i = 1; i < mText.size(ComposingText.LAYER1); ++i)
+				{
+					str = str + "|" + mText.toString(ComposingText.LAYER1, i, i);
+				}
+				show("layer1 = " + str);
+			}
+			{
+				String str = mText.toString(ComposingText.LAYER2, 0, 0);
+				for (int i = 1; i < mText.size(ComposingText.LAYER2); ++i)
+				{
+					str = str + "|" + mText.toString(ComposingText.LAYER2, i, i);
+				}
+				show("layer2 = " + str);
+			}
+
+			WnnWord word;
+			while ((word = mConvEngine.getNextCandidate()) != null)
+			{
+				show(word.candidate);
+			}
+
+			//mCandidateFilter.filter = CandidateFilter.FILTER_NON_ASCII;
+			for (int i = 0; i < mText.size(ComposingText.LAYER2); ++i)
+			{
+				if (mConvEngine.makeCandidateListOf(i) > 0 && (word = mConvEngine.getNextCandidate()) != null)
+				{
+					String str = word.candidate;
+					while ((word = mConvEngine.getNextCandidate()) != null)
+					{
+						str = str + "|" + word.candidate;
+					}
+					show("candidates for " + i + " = " + str);
+				}
+			}
+		}
+		catch (Exception e)
+		{
+			show(e.toString());
+			for (StackTraceElement el : e.getStackTrace())
+			{
+				show(el.toString());
+			}
+		}
+	}
+	
+	private void insert(String str)
+	{
+		mText.insertStrSegment(ComposingText.LAYER0, ComposingText.LAYER1, new StrSegment(str));
+		if (!mPreConv.convert(mText))
+		{
+			show("pre conv failed at " + mText.getCursor(ComposingText.LAYER0));
+		}
+	}
+	
+	private void show(String str)
+	{
+		mTextView.append("\n" + str);
+		//Toast.makeText(this, str, Toast.LENGTH_SHORT).show();		
+	}
 }
